d7487b93a6c5894f024349fc615bf7978dae619d
unknown <abhianil032@gmail.com>
2020-07-28 17:13:00 +0530

    nth commit

---
+++ js/hyp.js
@@ -0,0 +1,9 @@
+function popup()
+ {
+var mesg = document.getElementById("comment").value;
+alert(mesg);
+}
+function changecolor()
+{
+    document.getElementById("colorc").style.color="#2C8EC4";
+}
